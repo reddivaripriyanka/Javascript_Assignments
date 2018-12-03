5f9b0cbaa13ae7ea2d6550381b6bb08e464b850b
R <priyanka.i.r@capgemini.com>
2018-12-03 18:28:31 +0530

    files adds

---
+++ Nodejs_Assignmets1/add.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+     y = 3;
+     z = 4;
+     x =y+z;
+     console.log(x);
+   
+}
+myFunction();
+
+
---
+++ Nodejs_Assignmets1/biggest.js
@@ -0,0 +1,12 @@
+function biggest(){
+    var x=10;
+    var y=7;
+    var z=3;
+    if(x>y && x>z)
+    console.log(x);
+    else if(y>z)
+    console.log(y);
+    else
+    console.log(z);
+}
+biggest();
